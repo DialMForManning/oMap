599aab0183bfd4a6c4faf6f18f28a384b9acfdab
ALink2ThePast <btreelmanning@gmail.com>
2016-09-02 14:45:36 -0400

    Finish insert function

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -39,7 +39,7 @@ class SQLObject
     all_hashes = DBConnection.execute(<<-SQL)
       SELECT
         #{table_name}.*
-      from
+      FROM
         #{table_name}
     SQL
 
@@ -51,7 +51,16 @@ class SQLObject
   end
 
   def self.find(id)
-    # ...
+    attrs = DBConnection.execute(<<-SQL, id)
+      SELECT
+        #{table_name}.*
+      FROM
+        #{table_name}
+      WHERE
+        #{table_name}.id = ?
+    SQL
+
+    parse_all(attrs).first
   end
 
   def initialize(params = {})
@@ -69,11 +78,23 @@ class SQLObject
   end
 
   def attribute_values
-    # ...
+    self.class.columns.map { |value| self.send(value) }
   end
 
   def insert
-    # ...
+    cols_no_id = self.class.columns.drop(1)
+    col_names = cols_no_id.map(&:to_s).join(",")
+    question_marks = (["?"]*cols_no_id.length).join(",")
+
+    DBConnection.execute(<<-SQL, *attribute_values.drop(1))
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{question_marks})
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
+
   end
 
   def update
