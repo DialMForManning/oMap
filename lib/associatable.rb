37d2986b43d5459297520dbf0b0a952e0e21beb6
ALink2ThePast <btreelmanning@gmail.com>
2016-10-18 11:57:30 -0400

    refactor for code style

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -1,7 +1,6 @@
 require_relative 'searchable'
 require 'active_support/inflector'
 
-# Phase IIIa
 class AssocOptions
   attr_accessor(
     :foreign_key,
@@ -47,7 +46,6 @@ class HasManyOptions < AssocOptions
 end
 
 module Associatable
-  # Phase IIIb
   def belongs_to(name, options = {})
     self.assoc_options[name] = BelongsToOptions.new(name, options)
 
@@ -75,6 +73,37 @@ module Associatable
   def assoc_options
     @assoc_options ||= {}
     @assoc_options
+  end
+
+  def has_one_through(name, through_name, source_name)
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table_name = through_options.table_name
+      through_primary_key = through_options.primary_key
+      through_foreign_key = through_options.foreign_key
+
+      source_table_name = source_options.table_name
+      source_primary_key = source_options.primary_key
+      source_foreign_key = source_options.foreign_key
+
+      key_val = self.send(through_foreign_key)
+
+      search_results = DBConnection.execute(<<-SQL, key_val)
+        SELECT
+          #{source_table_name}.*
+        FROM
+          #{through_table_name}
+        JOIN
+          #{source_table_name} ON #{through_table_name}.#{source_foreign_key} =
+          #{source_table_name}.#{source_primary_key}
+        WHERE
+          #{through_table_name}.#{through_primary_key} = ?
+      SQL
+
+      source_options.model_class.parse_all(search_results).first
+    end
   end
 end
 
