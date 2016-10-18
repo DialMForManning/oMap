1026426a691c3f7b0eb2428c6144f055265f5153
ALink2ThePast <btreelmanning@gmail.com>
2016-10-18 16:58:09 -0400

    demo file with defined associations

---
+++ demo.rb
@@ -0,0 +1,25 @@
+require 'require_all'
+require_all 'lib'
+
+class Book < SQLObject
+  belongs_to :reader
+
+  self.make_helpers!
+end
+
+class Reader < SQLObject
+  belongs_to :library
+
+  has_many :books,
+    foreign_key: :reader_id
+
+    self.make_helpers!
+end
+
+class Library < SQLObject
+  has_many :readers
+
+  has_many :books
+
+  self.make_helpers!
+end
